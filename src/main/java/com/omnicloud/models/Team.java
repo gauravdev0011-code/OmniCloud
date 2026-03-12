31e811274c1870a1458cf6fe22769b3f3149cee4
Gaurav Dev <gauravdev0011@gmail.com>
2026-03-12 13:08:45 -0500

    Structure models

--- src/main/java/com/omnicloud/models/Team.java
+++ src/main/java/com/omnicloud/models/Team.java
@@ -1,4 +1,20 @@
 package com.omnicloud.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("teams")
 public class Team {
+
+    @Id
+    private Long id;
+    private String name;
+
+    public Team() {}
+    public Team(String name) { this.name = name; }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 }
