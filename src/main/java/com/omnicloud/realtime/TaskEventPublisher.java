5768a8c8367cf84e062b1e8f909853d22880faef
Gaurav Dev <gauravdev0011@gmail.com>
2026-03-22 09:37:14 -0500

    Upgrade event publisher to support structured realtime task events

--- src/main/java/com/omnicloud/realtime/TaskEventPublisher.java
+++ src/main/java/com/omnicloud/realtime/TaskEventPublisher.java
@@ -1,6 +1,5 @@
 package com.omnicloud.realtime;
 
-import com.omnicloud.models.Task;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
@@ -11,7 +10,6 @@ public class TaskEventPublisher {
     private final Sinks.Many<TaskEvent> sink =
             Sinks.many().multicast().onBackpressureBuffer();
 
-    // publish CREATE / UPDATE / DELETE
     public void publish(TaskEvent event) {
         sink.tryEmitNext(event);
     }
