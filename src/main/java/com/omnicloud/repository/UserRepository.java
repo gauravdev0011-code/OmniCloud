55e82e2dcb7a0527740b5520067e3230667f6a7c
Gaurav Dev <gauravdev0011@gmail.com>
2026-03-24 08:22:51 -0500

    Add UserRepository with email and username lookup

---
+++ src/main/java/com/omnicloud/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.omnicloud.repository;
+
+import com.omnicloud.models.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+
+    Mono<User> findByEmail(String email);
+
+    Mono<User> findByUsername(String username);
+}
