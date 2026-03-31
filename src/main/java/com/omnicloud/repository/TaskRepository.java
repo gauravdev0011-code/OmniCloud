691ea2f93963ae3cf1cab073b3e4030f4c4ab729
Gaurav Dev <gauravdev0011@gmail.com>
2026-03-30 23:14:27 -0500

    Add reactive TaskRepository

--- src/main/java/com/omnicloud/repository/TaskRepository.java
+++ src/main/java/com/omnicloud/repository/TaskRepository.java
@@ -2,8 +2,7 @@ package com.omnicloud.repository;
 
 import com.omnicloud.models.Task;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
 
-public interface TaskRepository extends ReactiveCrudRepository<Task, Long> {
-    Mono<Task> findById(Long id);
+public interface TaskRepository
+        extends ReactiveCrudRepository<Task, Long> {
 }
