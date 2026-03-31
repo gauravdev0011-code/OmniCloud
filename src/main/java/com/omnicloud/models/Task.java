8f195fa06b046a4b576ac165587d4c886e883e06
Gaurav Dev <gauravdev0011@gmail.com>
2026-03-30 23:11:47 -0500

    Fix Task entity mapping for R2DBC

--- src/main/java/com/omnicloud/models/Task.java
+++ src/main/java/com/omnicloud/models/Task.java
@@ -8,29 +8,38 @@ public class Task {
 
     @Id
     private Long id;
-    private String content;       // Task description
-    private Long assignedUserId;  // Which user this task is assigned to
-    private Long teamId;          // Which team this task belongs to
-    private String crdtState;     // CRDT state as JSON string
+
+    private String content;
+    private Long assignedUserId;
+    private Long teamId;
+    private String crdtState;
 
     public Task() {}
 
-    public Task(String content, Long assignedUserId, Long teamId, String crdtState) {
+    public Task(String content, Long assignedUserId,
+                Long teamId, String crdtState) {
         this.content = content;
         this.assignedUserId = assignedUserId;
         this.teamId = teamId;
         this.crdtState = crdtState;
     }
 
-    // Getters & setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
     public String getContent() { return content; }
     public void setContent(String content) { this.content = content; }
+
     public Long getAssignedUserId() { return assignedUserId; }
-    public void setAssignedUserId(Long assignedUserId) { this.assignedUserId = assignedUserId; }
+    public void setAssignedUserId(Long assignedUserId) {
+        this.assignedUserId = assignedUserId;
+    }
+
     public Long getTeamId() { return teamId; }
     public void setTeamId(Long teamId) { this.teamId = teamId; }
+
     public String getCrdtState() { return crdtState; }
-    public void setCrdtState(String crdtState) { this.crdtState = crdtState; }
+    public void setCrdtState(String crdtState) {
+        this.crdtState = crdtState;
+    }
 }
