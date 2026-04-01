ac97982ff06b80507cbd699b39ab3372b33c087f
Gaurav Dev <gauravdev0011@gmail.com>
2026-03-31 22:07:39 -0500

    Add reactive UserRepository with email lookup

--- src/main/resources/UserRepository.java
+++ src/main/resources/UserRepository.java
@@ -7,6 +7,4 @@ import reactor.core.publisher.Mono;
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
 
     Mono<User> findByEmail(String email);
-
-    Mono<User> findByUsername(String username);
 }
