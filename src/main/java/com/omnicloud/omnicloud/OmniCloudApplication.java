1a8869bf0644f6595385c0190b925d05bc4811f9
Gaurav Dev <gauravdev0011@gmail.com>
2026-04-05 11:40:10 -0500

    fix: correct package to com.omnicloud.omnicloud

---
+++ src/main/java/com/omnicloud/omnicloud/OmniCloudApplication.java
@@ -0,0 +1,11 @@
+package com.omnicloud.omnicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniCloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OmniCloudApplication.class, args);
+    }
+}
