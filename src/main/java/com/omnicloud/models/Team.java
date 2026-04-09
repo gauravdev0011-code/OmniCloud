adedc83076c25335e3e7e76bb85973e5e7d57455
Gaurav Dev <gauravdev0011@gmail.com>
2026-04-08 21:53:54 -0500

    fix: correct package, fix Team entity

--- src/main/java/com/omnicloud/models/Team.java
+++ src/main/java/com/omnicloud/models/Team.java
@@ -1,7 +1,10 @@
 package com.omnicloud.models;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
