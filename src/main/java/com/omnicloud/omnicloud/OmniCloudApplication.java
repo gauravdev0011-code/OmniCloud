613e781985ac435bc69ff3845e2e67b6b3ed64c1
Gaurav Dev <gauravdev0011@gmail.com>
2026-04-09 10:17:27 -0500

    fix: clean up OmniCloudApplication main class - removed redundant scanBasePackages - simplified package structure to com.omnicloud

--- src/main/java/com/omnicloud/omnicloud/OmniCloudApplication.java
+++ src/main/java/com/omnicloud/omnicloud/OmniCloudApplication.java
@@ -1,9 +1,9 @@
-package com.omnicloud.omnicloud;
+package com.omnicloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.omnicloud")
+@SpringBootApplication
 public class OmniCloudApplication {
     public static void main(String[] args) {
         SpringApplication.run(OmniCloudApplication.class, args);
