77d24f9862487bce79933baeab4e84db8e82bceb
Gaurav Dev <gauravdev0011@gmail.com>
2026-04-15 23:31:57 -0500

    feat: configure Spring Security for stateless JWT REST API and refactor TaskRequest DTO

--- src/main/java/com/omnicloud/dto/TaskRequest.java
+++ src/main/java/com/omnicloud/dto/TaskRequest.java
@@ -2,7 +2,15 @@ package com.omnicloud.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskRequest {
 
     @NotBlank(message = "Content cannot be empty")
@@ -16,45 +24,4 @@ public class TaskRequest {
 
     // optional field (kept nullable by design)
     private String crdtState;
-
-    public TaskRequest() {}
-
-    public TaskRequest(String content, Long assignedUserId, Long teamId, String crdtState) {
-        this.content = content;
-        this.assignedUserId = assignedUserId;
-        this.teamId = teamId;
-        this.crdtState = crdtState;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Long getAssignedUserId() {
-        return assignedUserId;
-    }
-
-    public void setAssignedUserId(Long assignedUserId) {
-        this.assignedUserId = assignedUserId;
-    }
-
-    public Long getTeamId() {
-        return teamId;
-    }
-
-    public void setTeamId(Long teamId) {
-        this.teamId = teamId;
-    }
-
-    public String getCrdtState() {
-        return crdtState;
-    }
-
-    public void setCrdtState(String crdtState) {
-        this.crdtState = crdtState;
-    }
 }
